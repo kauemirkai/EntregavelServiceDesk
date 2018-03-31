3b36b0788268fbedfa969da243937f1d170c08a4
kauemirkai <kauemirkai@192.168.0.14>
2018-03-31 18:02:46 -0300

    Aula 01- no login

---
+++ src/br/usjt/arqsw/service/FilaService.java
@@ -0,0 +1,28 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import br.usjt.arqsw.dao.FilaDAO;
+import br.usjt.arqsw.entity.Fila;
+/**
+ * @author Kauê Victor Paz Garcia Mirkai
+ * RA:81613004
+ * Professor:Bonato
+ * Turma:CCP3AN-MCA
+ */
+
+public class FilaService {
+	private FilaDAO dao;
+	
+	public FilaService() {
+		dao = new FilaDAO();
+	}
+	public ArrayList<Fila> listarFilas() throws IOException{
+		return dao.listarFilas();
+	}
+	
+	public Fila carregar(int id) throws IOException{
+		return dao.carregar(id);
+	}
+}
